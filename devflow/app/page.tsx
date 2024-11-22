5d8effeb9157bfd59f559848264fe756d5235c53
maciker <ikermacaya@gmail.com>
2024-11-22 10:55:20 +0100

    Devflow: fonts inter and space-grotesk added.

--- devflow/app/page.tsx
+++ devflow/app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <h1 className="text-4xl h1-bold font-inter">
+        Hello World with inter font
+      </h1>
+      <h1 className="text-4xl h1-bold font-space-grotesk">
+        Hello World with space-grotesk font
+      </h1>
     </div>
   );
 }
